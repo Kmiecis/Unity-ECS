1614b91ea437229b404954e1d229eaf1556fa252
Sławomir Kmiecik <33656921+Kmiecis@users.noreply.github.com>
2020-06-18 05:53:42 +0200

    Sin & Cos equations overrides

--- Assets/Scripts/Common/Equation3Solver.cs
+++ Assets/Scripts/Common/Equation3Solver.cs
@@ -16,7 +16,27 @@ namespace Common
 
 		void Solve()
 		{
+			if (!e1.IsValid() || !e2.IsValid() || !e3.IsValid())
+			{
+				return;
+			}
 
+			var ne1 = e1.WithNormalizedC();
+			var ne2 = e2.WithNormalizedC();
+			var ne3 = e3.WithNormalizedC();
+
+			var ne21 = (ne1 - ne2).WithZeroC();
+			var ne32 = (ne2 - ne3).WithZeroC();
+
+			if (!ne21.IsValid() || !ne32.IsValid())
+			{
+				return;
+			}
+
+			ne21.NormalizeB();
+			a = ne32.EvaluateA(ne21);
+			b = ne32.EvaluateB(a);
+			c = ne3.EvaluateC(a, b);
 		}
 
 #if UNITY_EDITOR
--- Assets/Scripts/CommonECS/Mathematics/Equations/SinEquation.cs
+++ Assets/Scripts/CommonECS/Mathematics/Equations/SinEquation.cs
@@ -18,7 +18,7 @@ namespace CommonECS.Mathematics
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static float Evaluate(float x, float dx = 0.0f, float sx = 1.0f, float dy = 0.0f, float sy = 1.0f)
 		{
-			return (math.sin((x + dx) * sx) + dy) * sy;
+			return (math.sin(((x + dx) * sx) * F) + dy) * sy;
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -26,5 +26,39 @@ namespace CommonECS.Mathematics
 		{
 			return Evaluate(x, f.dx, f.sx, f.dy, f.sy);
 		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static SinEquation FromPoints(float2 A, float2 B)
+		{
+			// TODO
+			return new SinEquation();
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool Equals(SinEquation other)
+		{
+			return this.dx == other.dx && this.sx == other.sx && this.dy == other.dy && this.sy == other.sy;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public override bool Equals(object obj)
+		{
+			return obj is SinEquation && Equals((SinEquation)obj);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public override int GetHashCode()
+		{
+			return dx.GetHashCode() ^ sx.GetHashCode() ^ dy.GetHashCode() ^ sy.GetHashCode();
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public override string ToString()
+		{
+			return string.Format("y = (sin((x + {0}) * {1}) + {2}) * {3}", dx, sx, dy, sy);
+		}
+
+		const float F = math.PI * 0.5f;
+		const float RF = 1.0f / F;
 	}
 }
