6d822667b1b27ca6b00fa035146abb2f2de6ef38
Sławomir Kmiecik <33656921+Kmiecis@users.noreply.github.com>
2020-10-29 07:08:00 +0100

    Added com.unity.animations

---
+++ Assets/Scripts/CommonECS/Common/AuthoringBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace CommonECS
+{
+	public abstract class AuthoringBehaviour : MonoBehaviour, IConvertGameObjectToEntity
+	{
+		public abstract void Convert(Entity entity, EntityManager entityManager, GameObjectConversionSystem conversionSystem);
+	}
+}
